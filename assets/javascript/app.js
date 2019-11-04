5a778e0c735696baaf48a7ea240ab8aaa8772abc
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2019-11-04 16:17:12 -0700

    Added initial javascript and jquery logic. Added calls to giphy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,113 @@
-var newButton = $("<button>");
-newButton.text("New Button");
-$("#button-area").append(newButton);
+var items = ["skiing",
+    "mountain biking",
+    "hiking",
+    "weight lifting",
+    "kayaking",
+    "rock climbing",
+    "back packing",
+    "snow boarding"
+];
+
+var test;
+
+var gifTastic = {
+    items: items,
+    title: "Gif-tastic",
+    newImage: "",
+
+    // Create a button for each item in an array
+    createButtons: function () {
+        for (i = 0; i < this.items.length; i++) {
+            var newButton = $("<button data-subject=\"" + this.items[i] + "\" class=\"btn\">");
+            console.log(newButton);
+            newButton.text(this.items[i]);
+            $("#button-area").append(newButton);
+        }
+        var gifArea = $("<div id=\"gif-area\">gifArea</div>");
+        $("#container").append(gifArea);
+        // $("#gif-area").prepend("<img src=\"https://media2.giphy.com/media/8rFQp4kHXJ0gU/200_s.gif\"></img>");
+
+    },
+
+    getGiphys: function (subject, limit, rating) {
+        // Create our query string
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=PU1WmxlDY9rk5q1lVFBzJf1OqqXpXdGr&q=" + subject + "&limit=" + limit + "&offset=0&rating=" + rating + "&lang=en";
+        console.log(queryURL);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            console.log("img html should look like: https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif\" data-still=\"https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif\" data-animate=\"https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif\" data-state=\"still\" class=\"gif\">");
+            var srcImg = response.data[0];
+            var imgHtml = "<img src=\"" + srcImg.images.fixed_height_still.url + "\" data-still=\"" + srcImg.images.fixed_height_still.url + " data-animate=\"" + srcImg.images.fixed_height_still.url + "\"";
+            console.log("getGiphys(): imgHtml: " + imgHtml);
+
+            $("#gif-area").prepend($(imgHtml));
+
+        });
+
+    }
+};
+
+// Create initial page
+gifTastic.createButtons();
+
+//
+// Handle these click events
+//
+
+// Subject buttons
+$(document).on("click", ".btn", function () {
+    // Display the gifs
+    var subject = $(this).attr("data-subject");
+    var limit = 1;
+    var rating = "G";
+
+    console.log("Subject = " + subject);
+    // gifTastic.getGiphys(subject, "1", "G");
+
+    // Create our query string
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=PU1WmxlDY9rk5q1lVFBzJf1OqqXpXdGr&q=" + subject + "&limit=" + limit + "&offset=0&rating=" + rating + "&lang=en";
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        // console.log("img html should look like: https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif\" data-still=\"https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200_s.gif\" data-animate=\"https://media1.giphy.com/media/3o85xkQpyMlnBkpB9C/200.gif\" data-state=\"still\" class=\"gif\">");
+        var srcImg = response.data[0];
+        var imgHtml = "<img src=\"" + srcImg.images.fixed_height_still.url + "\" data-still=\"" + srcImg.images.fixed_height_still.url + "\" data-animate=\"" + srcImg.images.fixed_height.url + "\" data-state=\"still\" class=\"gif\"></img>";
+        console.log("imgHtml: " + imgHtml);
+
+        newImage = $(imgHtml);
+        $("#gif-area").prepend(newImage);
+
+    });
+
+    // $("#gif-area").prepend("<img src=https://media3.giphy.com/media/rMszO5FZRLMyc/200_s.gif?cid=9ab9b4a95d847a68740442c80e2349788ecf734060b69225&rid=200_s.gif</img>");
+    // $("#gif-area").prepend(newImage);
+});
+
+// gif clicks
+$(document).on("click", ".gif", function () {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+
+        $(this).attr("src", $(this).attr("data-animate"));
+        console.log("Setting data-animate to " + $(this).attr("data-animate"));
+
+        $(this).attr("data-state", "animate");
+        console.log("Setting data-state to " + $(this).attr("data-state"));
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        console.log("Set animate to " + $(this).attr("data-still"));
+
+        $(this).attr("data-state", "still");
+        console.log("Setting data-state to " + $(this).attr("data-state"));
+    }
+});
